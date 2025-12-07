7c9b31dd877f582b46f6e3b0bc6bafdae68e6cff
birukKE <158283227+birukKE@users.noreply.github.com>
2025-12-07 22:40:36 +0100

    Clean up main.jsx by removing commented code
    Removed commented-out code for cleaner main.jsx.

--- web_client/src/main.jsx
+++ web_client/src/main.jsx
@@ -13,29 +13,3 @@ createRoot(document.getElementById('root')).render(
      </Router> 
   </StrictMode>,
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter as Router } from "react-router-dom"; // or HashRouter
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <Router>
-//       <App />
-//     </Router>
-//   </StrictMode>,
-// )
