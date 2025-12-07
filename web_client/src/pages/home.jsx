c59ec3ff3a0638827c352bb7bbea46a1cc12165b
birukKE <158283227+birukKE@users.noreply.github.com>
2025-12-07 21:01:00 +0100

    Refactor Home component and clean up imports

--- web_client/src/pages/home.jsx
+++ web_client/src/pages/home.jsx
@@ -1,9 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
-// import {Toggle} from '/src/components/toggle'
 import Toggle from '../components/toggle.jsx';
-// import TextField from '../components/textField.jsx';
 import TextField from "../components/textField";
 import './page.css'
 import Loader from "../components/loader"
@@ -14,7 +12,6 @@ import TextStyle from "../components/textStyle"
 
 const Row = (props) =>{
     const {movieImages, movieTitle} = props
-    console.log("to print table images: ", movieImages)
     return(
         <tr>
             <td className="table-data"> <img  src = {movieImages} alt = "No image" className="movie__image"/> </td> 
@@ -26,27 +23,15 @@ const Row = (props) =>{
 
 const Table = (props) =>{
     const {movieListData, movieImageUrlsData} = props
-    console.log("Table movieListData = ", movieListData)
-    // console.log("typeof movieList =", typeof movieListData);
-    // if(movieListData.length == 1){
-    //     alert(movieListData[0])
-    // }
-    // let tempMovieListData = []
-    // if(movieListData.length > 0){
-    //     tempMovieListData = JSON.parse(movieListData)
-    // }
     let tempMovieListData = movieListData
     return(
         <table className="table table-striped">
             <tbody>
-                {/* <tr>
                     <td>Movies</td>
                     <td>Picture</td>
-                </tr> */}
 
                      {tempMovieListData.map((row, index) => 
                         {
-                            console.log("row", row)
                         return <Row
                             key = {index}
                             movieTitle={row}
@@ -63,16 +48,12 @@ const Table = (props) =>{
 const handleApiRequest = async(titleOfMovie) =>{ 
         const apiKey = "6720af32"
         let tempImage = ""
-        console.log("titleOfMovie = ", titleOfMovie)
-
         try{
             let res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${titleOfMovie}`)
             const data = await res.json()
             tempImage = data.Poster
-            console.log("poster: ", tempImage)
         }catch(error) 
             {
-                console.error(`ERROR! ${error}`)
             }
         return tempImage
     }
@@ -98,27 +79,22 @@ export function Home(){
     const [toggleState, setToggleState] = useState(false)
     const [isButtonClicked, setIsButtonClicked] = useState(false)
     const [isMovieFound, setIsMovieFound] = useState(true)
-    // const [valState, setValState] = useState(false)
-    // const [isDescription, setIsDescription] = useState()
-
     
     const postValToBackend = () => {
         setIsButtonClicked(true)
         if(val !== ""){
                 fetch(`/api/handleUserInput`, {
                 method: "POST",
-                headers: {"Content-Type": "application/json"}, // We are telling serever that the kind of data we are sending with this request is a json data
+                headers: {"Content-Type": "application/json"}, 
                 body: JSON.stringify({"value" : val, "isDescription": toggleState})
             })
-            .then(res => res.json())   // res.json() does not immediately give you the data. Instead, it returns a Promise that will eventually resolve to the parsed JSON.
+            .then(res => res.json())   
             .then(myData =>{
                 
                 let parsed = myData
-                console.log("AAA: ", parsed)
                 if (typeof myData === "string") {
                     parsed = JSON.parse(myData);
                 }
-                console.log("AAABBB: ", parsed)
                 setMovieList(parsed)
                 if(parsed.length > 0){
                     setVal("")
@@ -127,17 +103,14 @@ export function Home(){
                 }else if(myData.length === 0){
                     setIsMovieFound(false)
                 }
-                console.log("movieList postVal: ", movieList)
                 })
             .catch(error => { console.error(`ERROR! ${error}`)})}
-            // setIsButtonClicked(false)
 
     }
 
 
     const getImage = async(filmTitle) =>{
         const tempImage = await (handleApiRequest(filmTitle))
-        console.log("tempImage***************************: ", tempImage)
         return tempImage
     }
 
@@ -148,10 +121,8 @@ export function Home(){
         const extractingListItems = async() =>{
             const urls = await fetAllImageUrls()
             setMovieImageUrls(urls)
-            console.log("Yeah, ", movieList)
         }
         extractingListItems()
-        console.log("movieImageUrls -------------------- ", movieImageUrls)
     }, [movieList])
 
     useEffect(() => {
@@ -179,7 +150,6 @@ export function Home(){
 
 
     return(
-        // <div className="container-class">
             <div className={`container-class ${movieList.length > 0?"change":""}`}>
                 <span className="top-span"></span>
                 <TextStyle/>
@@ -201,11 +171,11 @@ export function Home(){
                 <Table  movieListData = {movieList}  movieImageUrlsData = {movieImageUrls}/> 
                 <span className="bottom-span"></span>
                 
-            {/* </div> */}
         </div>
     )
 
 }
+
 
 
 
